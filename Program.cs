88dfadde0587ef098297d23823d2173c13d53c80
yavuzkocar <87602474+yavuzkocar@users.noreply.github.com>
2021-10-20 17:43:08 +0300

    console programlama ilk ders tekrarı

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("isminizi giriniz");
+            String name = Console.ReadLine();
+            Console.WriteLine("soyisminizi giriniz");
+            String surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
